4728f847e0386dd6507aa87b9eb15bc1cee10511
abarnhill <alex@alexanderbarnhill.com>
2020-04-22 17:06:23 +0200

    Initial Commit with hello world

---
+++ clinkle/src/main/java/com/amos/clinkle/App.java
@@ -0,0 +1,13 @@
+package com.amos.clinkle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
