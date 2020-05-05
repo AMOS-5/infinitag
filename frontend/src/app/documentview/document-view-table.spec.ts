46f6cbca6e521459905fdca87f6831445bbcbd28
Markus Wich <markus.wich@fau.de>
2020-05-05 16:09:39 +0200

    added testcase for angular component

---
+++ frontend/src/app/documentview/document-view-table.spec.ts
@@ -0,0 +1,47 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DocumentViewTable } from './document-view-table';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSortable, MatSortModule} from '@angular/material/sort';
+
+describe('DocumentViewTable', () => {
+  let component: DocumentViewTable;
+  let fixture: ComponentFixture<DocumentViewTable>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatSortModule,
+      ],
+      declarations: [DocumentViewTable]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DocumentViewTable);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('test received data', () => {
+    //TODO extend testcase once real data is read
+    expect(component.dataSource).toBeTruthy();
+  });
+
+  it('test sorting data', () => {    
+    //tests sorting functionality by comparing first and last row before and after sorting
+    component.sort.sort(({ id: 'name', start: 'asc'}) as MatSortable);
+    const dataLength = component.dataSource.data.length;
+    let firstrow = component.dataSource.sortData[0];
+    let lastrow = component.dataSource.sortData[dataLength-1];
+    component.sort.sort(({ id: 'name', start: 'asc'}) as MatSortable);
+    expect(firstrow).toEqual(component.dataSource.sortData[dataLength-1]);
+    expect(lastrow).toEqual(component.dataSource.sortData[0]);
+  });
+
+});
