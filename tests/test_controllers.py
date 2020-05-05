0b3981b4f1ad31d5d046da4a017e6852d61bfbe4
abarnhill <alex@alexanderbarnhill.com>
2020-05-05 19:20:13 +0200

    Fix formatting in test_controllers

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,35 +1,33 @@
 import json
 import unittest
-from Scripts.app import app
+
+from app import app
 
 
 class BasicTestCase(unittest.TestCase):
-     def test_home(self):
-         tester = app.test_client(self)
-         response = tester.get('/', content_type='html/text')
-         self.assertEqual(response.status_code, 200)
-         self.assertEqual(response.data, b'Hello World!')
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello World!')
 
-     def test_health(self):
-         tester = app.test_client(self)
-         response = tester.get('/health', content_type="application/json")
-         self.assertEqual(response.status_code, 200)
-         data_response = json.loads(response.data)
-         health = data_response['status']
-         self.assertEqual(health, "UP")
+    def test_health(self):
+        tester = app.test_client(self)
+        response = tester.get('/health', content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        data_response = json.loads(response.data)
+        health = data_response['status']
+        self.assertEqual(health, "UP")
 
-     def test_documents(self):
-         tester = app.test_client(self)
-         response = tester.get('/documents', content_type="application/json")
-         self.assertEqual(response.status_code, 200)
+    def test_documents(self):
+        tester = app.test_client(self)
+        response = tester.get('/documents', content_type="application/json")
+        self.assertEqual(response.status_code, 200)
 
-         data_response = json.loads(response.data)
-         documents = data_response['documents']
-         self.assertIsNotNone(documents)
-
-
+        data_response = json.loads(response.data)
+        documents = data_response['documents']
+        self.assertIsNotNone(documents)
 
 
 if __name__ == '__main__':
     unittest.main()
-
