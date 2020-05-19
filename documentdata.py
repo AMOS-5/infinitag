79386180f95196212be841a3c3f4cb0ff7a72d1d
Markus Wich <markus.wich@fau.de>
2020-05-19 22:50:34 +0200

    added documentdata.py

---
+++ documentdata.py
@@ -0,0 +1,47 @@
+"""
+Module handling the document metadata
+"""
+from datetime import datetime
+from typing import List
+
+class DocumentData:
+    """
+    Class encapsuling the metadata of a document file
+    """
+
+    def __init__(self, name: str, path: str, type: str, lang: str, size: int, createdAt: datetime, tags: List[str]):
+        """
+        Constructs a new DocumentData object
+
+        :param name: Filename as string
+        :param path: Filepath as string
+        :param type: Filetype as string
+        :param lang: Language of the document as string
+        :param size: Size of the document as int
+        :param createdAt: Creationdate of document as datetime object
+        :param tags: Tags as a list of strings
+        :return:
+        """
+        self.name = name
+        self.path = path
+        self.type = type
+        self.lang = lang
+        self.size = size
+        self.createdAt = createdAt
+        self.tags = tags
+
+    def as_dict(self):
+        """
+        Inserts relevant member variables into a dictionary and returns it
+        (calls .ctime() on datetime variables to convert them to strings)
+        :return: dictionary with variable names as keys
+        """
+        return {
+            "name" : self.name,
+            "path" : self.path,
+            "type" : self.type,
+            "lang" : self.lang,
+            "size" : self.size,
+            "createdAt" : self.createdAt.ctime(),
+            "tags" : self.tags
+        }
