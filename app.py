092bff42099068d97797fe5fbaa7cd7213bca759
Markus Wich <markus.wich@fau.de>
2020-06-01 15:49:32 +0200

    connected dimension/keyword funtions to solr

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def upload_file():
 
 
 
-@app.route('/changetags', methods=['PATCH'])
-def change_tags():
+@app.route('/changekeywords', methods=['PATCH'])
+def change_keywords():
     try:
         iDoc = request.json
         id = iDoc.get('id')
@@ -71,7 +71,7 @@ def change_tags():
     except Exception as e:
         return jsonify(f"Bad Gateway to solr: {e}"), 502
 
-    print('changed tags on file ' + id + ' to ' + ','.join(keywords) , file=sys.stdout)
+    print('changed keywords on file ' + id + ' to ' + ','.join(keywords) , file=sys.stdout)
     return jsonify("success"), 200
 
 
@@ -92,8 +92,47 @@ def get_health():
     return jsonify({"status": "UP"})
 
 
-@app.route('/tags', methods=['GET', 'POST'])
-def tags():
+
+@app.route('/dims', methods=['GET', 'POST'])
+def dimensions():
+    """
+    Handles GET and POST request for uncategorized dimensions
+    :return: json object containing the uncategorized dimensions and/or a status message
+    """
+    if request.method == 'GET':
+        try:
+            data = solr.dimensions.get()
+            return jsonify(data), 200
+        except Exception as e:
+            return jsonify(f"internal error: {e}"), 500
+    elif request.method == 'POST':
+        try:
+            data = request.json.get('dim')
+            solr.dimensions.add(data)
+            return jsonify(data + " has been added to dimensions"), 200
+        except Exception as e:
+            log.error(f"/dims: {e}")
+            return jsonify(f"/dims internal error: {e}"), 500
+
+
+@app.route('/dims/<dim_id>', methods=['DELETE'])
+def remove_dimension(dim_id):
+    """
+    Handles DELETE request for uncategorized dimensions
+    :return: json object containing a status message
+    """
+    try:
+        solr.dimensions.delete(dim_id)
+        return jsonify(f"{dim_id} has been removed from dimensions"), 200
+    except Exception as e:
+        return jsonify(f"{dim_id} internal error: {e}"), 500
+
+@app.route('/keys', methods=['GET', 'POST'])
+def keywords():
+    """
+    Handles GET and POST request for uncategorized keywords
+    :return: json object containing the uncategorized keywords and/or a status message
+    """
     if request.method == 'GET':
         try:
             data = solr.keywords.get()
@@ -102,82 +141,23 @@ def tags():
             return jsonify(f"internal error: {e}"), 500
     elif request.method == 'POST':
         try:
-            data = request.json.get('tag')
-            solr.keywords.add(data)
-            return jsonify(data + " has been added"), 200
-        except Exception as e:
-            log.error(f"/documents: {e}")
-            return jsonify(f"/tags internal error: {e}"), 500
-
-
-@app.route('/tags/<tag_id>', methods=['DELETE'])
-def remove_tags(tag_id):
-    try:
-        solr.keywords.delete(tag_id)
-        return jsonify(f"{tag_id} has been removed"), 200
-    except Exception as e:
-        return jsonify(f"{tag_id} internal error: {e}"), 500
-
-dims_arr = ["country", "state", "city"]
-keys_arr = ["germany", "bavaria", "erlangen"]
-
-@app.route('/dims', methods=['GET', 'POST'])
-def dims():
-    if request.method == 'GET':
-        try:
-            data = dims_arr
-            #TODO query here
-            return jsonify(data), 200
-        except Exception as e:
-            return jsonify(f"internal error: {e}"), 500
-    elif request.method == 'POST':
-        try:
-            data = request.json.get('dim')
-            #TODO add here
-            dims_arr.append(data)
-            log.critical(f"/dim: {data}")
-            return jsonify(data + " has been added"), 200
-        except Exception as e:
-            log.error(f"/dims: {e}")
-            return jsonify(f"/dims internal error: {e}"), 500
-
-
-@app.route('/dims/<dim_id>', methods=['DELETE'])
-def remove_dim(dim_id):
-    try:
-        #TODO delete here
-        dims_arr.remove(dim_id)
-        return jsonify(f"{dim_id} has been removed"), 200
-    except Exception as e:
-        return jsonify(f"{dim_id} internal error: {e}"), 500
-
-@app.route('/keys', methods=['GET', 'POST'])
-def keys():
-    if request.method == 'GET':
-        try:
-            data = keys_arr
-            #TODO query here
-            return jsonify(data), 200
-        except Exception as e:
-            return jsonify(f"internal error: {e}"), 500
-    elif request.method == 'POST':
-        try:
             data = request.json.get('key')
-            #TODO add here
-            keys_arr.append(data)
-            log.critical(f"/key: {data}")
-            return jsonify(data + " has been added"), 200
+            solr.keywords.add(data)
+            return jsonify(data + " has been added to keywords"), 200
         except Exception as e:
             log.error(f"/dims: {e}")
             return jsonify(f"/dims internal error: {e}"), 500
 
 
 @app.route('/keys/<key_id>', methods=['DELETE'])
-def remove_key(key_id):
+def remove_keyword(key_id):
+    """
+    Handles DELETE request for uncategorized keywords
+    :return: json object containing a status message
+    """
     try:
-        #TODO delete here
-        keys_arr.remove(key_id)
-        return jsonify(f"{key_id} has been removed"), 200
+        solr.keywords.delete(key_id)
+        return jsonify(f"{key_id} has been removed from keywords"), 200
     except Exception as e:
         return jsonify(f"{key_id} internal error: {e}"), 500
 
