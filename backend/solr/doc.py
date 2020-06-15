d68c8f1f2692b35d08121094000d7e114116fa5f
Markus Wich <markus.wich@fau.de>
2020-06-15 15:11:18 +0200

    fixed bug

--- backend/solr/doc.py
+++ backend/solr/doc.py
@@ -143,20 +143,7 @@ class SolrDoc:
     def as_dict(self, keywords_as_str: bool = False) -> dict:
         return {
             "id": self.id,
-            "keywords": [kw.as_str() for kw in self.keywords],
-            "title": self.title,
-            "type": self.file_type,
-            "language": self.lang,
-            "size": self.size,
-            "creation_date": self.creation_date,
-            "content": self.content,
-        }
-
-    #TODO better name
-    def as_dict_but_keywords_are_dicts(self) -> dict:
-        return {
-            "id": self.id,
-            "keywords": [kw.as_dict() for kw in self.keywords],
+            "keywords": [kw.as_str() if keywords_as_str else kw.as_dict() for kw in self.keywords],
             "title": self.title,
             "type": self.file_type,
             "language": self.lang,
