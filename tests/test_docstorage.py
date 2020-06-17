e04c21bde97966dc3d6a3906f70c24fe833ea1ea
Dmitrij VInokour <dimfred.1337@web.de>
2020-06-17 12:19:09 +0200

    changed ec2 endpoint

--- tests/test_docstorage.py
+++ tests/test_docstorage.py
@@ -9,7 +9,7 @@ class DocStorageTestCase(unittest.TestCase):
     config = {
         "corename": "test_documents",
         # "url": "http://localhost:8983/solr/",
-        "url": "http://ec2-52-87-180-131.compute-1.amazonaws.com:8983/solr",
+        "url": "http://ec2-3-84-34-102.compute-1.amazonaws.com:8983/solr",
         "always_commit": True,
     }
 
