ddfa69852112aede03e165b3531bad8eacaa737b
Nazmus Sakib <nazmussakib@MacBook-Pro.local>
2020-07-04 11:18:55 +0200

    started workingon dashboard

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -36,11 +36,78 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent{
+  theme: string;
+  pieOptions = {
+    title: {
+      text: 'Tagged/Untagged Documents',
+      x: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+      x: 'center',
+      y: 'bottom',
+      data: ['Tagged Documents', 'Untagged Documents']
+    },
+    calculable: true,
+    series: [
+      {
+        name: 'area',
+        type: 'pie',
+        radius: [70, 110],
+        data: [
+          { value: 10, name: 'Tagged Documents' },
+          { value: 90, name: 'Untagged Documents' }
+        ]
+      }
+    ]
+  };
 
-  constructor() { }
+  barOptions = {
+    color: ['#3398DB'],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    yAxis: [{
+      type: 'value'
+    }],
+    series: [{
+      name: 'Documents Added',
+      type: 'bar',
+      barWidth: '60%',
+      data: [3, 5, 10, 3, 5, 2, 4]
+    }]
+  };
 
-  ngOnInit(): void {
+  options = [
+    {value: 'days', viewValue: 'Days (last 7 days)'},
+    {value: 'Weeks', viewValue: 'Weeks (last 4 weeks)'},
+    {value: 'months', viewValue: 'Months (last 12 months)'},
+    {value: 'years', viewValue: 'Years (since start)'}
+  ];
+  changeBar = (event) => {
+    console.log("here")
+    console.log(event.target.value)
   }
-
 }
