1192519e4837dc3300d05c35971e1d457b9d757d
Dmitrij Vinokour <dimfred.1337@web.de>
2020-07-18 10:17:41 +0200

    rm log, change tagcloud to use statistics

--- utils/tagcloud.py
+++ utils/tagcloud.py
@@ -1,38 +1,21 @@
-import ast
 from backend.service import SolrService
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from pathlib import Path
-from backend.solr import (
-    SolrDocuments,
-    config
-)
-import os
+
+from wordcloud import WordCloud
 from os import path
 
-def update_tagcloud(path_to_save, solrService):
-    q_keywords = solrService.docs.search("*:*", fl='keywords')
-    docs = q_keywords.docs
-    all_keywords =[]
+def update_tagcloud(path_to_save: str, solr_service: SolrService) -> None:
+    max_words = 1000
 
-    for dictionaries in docs:
-        for keywords,list_values in dictionaries.items():
-            for list_contents in list_values:
-                convert_dictionary = ast.literal_eval(list_contents)
-                all_keywords.append(convert_dictionary['value'])
+    keywords = solr_service.keyword_statistics.keywords(rows=max_words)
+    keywords_str = " ".join(kw for kw in keywords)
 
-    distinct = list(dict.fromkeys(all_keywords))
-    string = " ".join(str(x) for x in distinct)
-    #print(string)
-    if len(distinct) > 0:
-        wordcloud = WordCloud(width=1200 , height=700,max_font_size=100, max_words=1000,
-                              background_color="white").generate(string)
-        wordcloud.to_file(path.join(path_to_save, 'tag_cloud.png'))
+    if len(keywords) > 0:
+        wordcloud = WordCloud(
+            width=1200,
+            height=700,
+            max_font_size=100,
+            max_words=max_words,
+            background_color="white",
+        ).generate(keywords_str)
 
-    #Saved image in the Assets folder so it can be updated everytime
-    # plt.figure()
-    # plt.axis("off")
-    # plt.imshow(wordcloud, interpolation="bilinear")
-    # print('Printing word cloud now..')
-
-
+        wordcloud.to_file(path.join(path_to_save, "tag_cloud.png"))
